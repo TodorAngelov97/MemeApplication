6ec3de2802c3efea7a10f398f737c39820c1faab
Todor <todorangelov_97@abv.bg>
2019-05-12 19:03:43 +0300

    Added create-meme template

--- meme-application/src/app/app-routing.module.ts
+++ meme-application/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MemeGridComponent } from './main-container/meme-grid/meme-grid.component';
-
+import { CreateMemeComponent } from './main-container/create-meme/create-meme.component';
 
 
 const routes: Routes = [
   { path: '', component: MemeGridComponent, pathMatch: 'full' },
+  { path: 'create-meme', component: CreateMemeComponent },
 ];
 
 
--- meme-application/src/app/mock-memes.ts
+++ meme-application/src/app/mock-memes.ts
@@ -6,9 +6,10 @@ export const MEMES: Meme[] = [
     { id: 2, title: ' Liolo', imagePath: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUDA5YyP2kCk8auY7EbhUP9Ux-iVd2AA5iD5ipOYBjJ2WnOTdg' },
     { id: 3, title: ' Eee ne', imagePath: 'https://i.ytimg.com/vi/ygdzjB0Ka5w/maxresdefault.jpg' },
     { id: 4, title: ' Male', imagePath: 'https://media0.giphy.com/media/ypX8YZszkIXFC/giphy.gif' },
-    { id: 5, title: ' k', imagePath: 'Mr. Nice' },
+    // tslint:disable-next-line: max-line-length
+    { id: 5, title: ' k', imagePath: 'https://cdn.cnn.com/cnnnext/dam/assets/171122130029-10-reasons-to-visit-plovdiv---plovdiv-main-street-full-169.jpg' },
     { id: 6, title: ' Vui vui', imagePath: 'https://negramoten.000webhostapp.com/wp-content/uploads/2017/03/Vyi.jpg' },
     { id: 7, title: ' Sylza', imagePath: 'https://i.ytimg.com/vi/ki10twOf6zQ/maxresdefault.jpg' },
     { id: 8, title: ' Sq si', imagePath: 'https://memegenerator.net/img/instances/67684217.jpg' },
-    { id: 9, title: ' Nqma kak', imagePath: '' }
+    { id: 9, title: ' Nqma kak', imagePath: 'https://media2.giphy.com/media/Hlc2TPGlDYZFK/source.gif' }
 ];
