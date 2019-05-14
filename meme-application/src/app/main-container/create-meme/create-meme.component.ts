c0e6e4619813f1966c557b0202bd989c31266fb6
Todor <todorangelov_97@abv.bg>
2019-05-14 13:35:24 +0300

    Added http client logic in create meme

--- meme-application/src/app/main-container/create-meme/create-meme.component.ts
+++ meme-application/src/app/main-container/create-meme/create-meme.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Meme } from '../../models/meme.model';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-create-meme',
@@ -8,19 +9,30 @@ import { Meme } from '../../models/meme.model';
   styleUrls: ['./create-meme.component.scss']
 })
 export class CreateMemeComponent implements OnInit {
+  constructor(private http: HttpClient) { }
 
-  memeForm = new FormGroup({
-    title: new FormControl(''),
-    image: new FormControl(''),
-  });
+  memeForm: FormGroup;
+  url: String = 'http://localhost:8080';
+  createMemeForm() {
+    return new FormGroup({
+      title: new FormControl('', [Validators.required]),
+      image: new FormControl('', [Validators.required]),
+    });
+  }
 
-  constructor() { }
+
 
   ngOnInit() {
+    this.memeForm = this.createMemeForm();
   }
 
   onSubmit() {
 
+    const formData = new FormData();
+    formData.append('title', this.memeForm.get('title').value);
+    formData.append('file', this.memeForm.get('image').value);
+
+    return this.http.post<Meme>(this.url + '/kurec', formData).subscribe();
 
   }
 }
